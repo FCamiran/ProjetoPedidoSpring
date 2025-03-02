256f76c1d558152b3a6a20ed8718af5955a6e9c7
Fernando Camiran <fernando.s.camiran@gmail.com>
2025-03-02 16:32:27 -0300

    Banco de dados h2 e criacao automatica de dados

--- first-project-spring/src/main/java/com/camiran/first_project_spring/FirstProjectSpringApplication.java
+++ first-project-spring/src/main/java/com/camiran/first_project_spring/FirstProjectSpringApplication.java
@@ -2,8 +2,10 @@ package com.camiran.first_project_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.camiran.first_project_spring.domain")
 public class FirstProjectSpringApplication {
 
 	public static void main(String[] args) {
