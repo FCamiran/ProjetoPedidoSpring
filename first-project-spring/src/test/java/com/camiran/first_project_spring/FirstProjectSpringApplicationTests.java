a6c0b8b4e43bce9a7029583acd997fe51a023c4d
Fernando Camiran <fernando.s.camiran@gmail.com>
2025-03-02 14:46:14 -0300

    Rest testado

---
+++ first-project-spring/src/test/java/com/camiran/first_project_spring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camiran.first_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
